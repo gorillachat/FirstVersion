d1f1f0f4b8fae6906e38185780f4e5652614b993
srei <steferu@gmail.com>
2016-10-27 22:15:41 -0700

    Added sequelize to package.json. Established connection to port 5432, authenticated connection to database. Created main 3 models: ROOM, MSG, and USER.

--- Schemas/Tables.js
+++ Schemas/Tables.js
@@ -3,8 +3,19 @@
 //require sequelize
 const Sequelize = require('sequelize');
 
+//setting up a connection pool on initialization for now because we are connecting from a single process. If we want to connect to DB from multiple processes, we will have to create an instance PER process (code will need to be modified to add a max connection pool size etc)
 const sequelize = new Sequelize('postgres://localhost:5432/ChatRoomTables');
 
+//authentication of connection
+sequelize.authenticate()
+	.then( err => {
+		console.log('Connection on database established');
+	})
+	.catch( err =>{
+		console.log('Unable to connect to database',err);
+	});
+
+//Room
 const Room
 
 const Msg

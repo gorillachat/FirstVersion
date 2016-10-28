fc59ee6a2a0c569fb396d1092a133ee67eaeed07
ryanbas21 <ryanbas21@gmail.com>
2016-10-27 21:53:39 -0700

    setup routes for get and post, added some logic for the postMessage, no db schema

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -3,7 +3,28 @@
 
 
 module.exports = {
-  postMessage: () => {
 
+  postMessage: (req,res,next) => {
+    res.setHeader('content-type', 'application/json');
+    //set headers
+    const roomID = req.params.id;
+    //parse req.body and save Object as headers
+    const headers = JSON.parse(req.body);
+    
+
+    //destructuring the req.body which contains
+    //creator, time , room, and messageBody (the message itself)
+    //each key:value is set to a constant
+    //ex: const creator = ryan, time = 12:30, room = 3, msgBody = 'this is an ex'
+    const { creator, time, room, msgBody } = headers;
+
+    //store to database
+
+
+
+
+
+    //call next
+    next();
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const {postMessage} = require('./controllers/messageController.js');
+const {getRooms} = require('./controllers/roomController.js');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 
 // Create our app
 const app = express();
-
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static('public'));
 
+//Express route to get list of rooms in a nearby area
+//responds with list of rooms
+app.get('/roomlist', getRooms)
 
-app.post('/rooms/:roomid', )
+
+//Express route for saving message from specfic room:id
+app.post('/rooms/:roomid', postMessage )
 
 
 
@@ -15,6 +25,4 @@ app.post('/rooms/:roomid', )
 
 
 //listening on port 3000
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
-});
+app.listen(3000, () => console.log('Express server is up on port 3000'));

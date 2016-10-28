439222deb5321234716e81fac3b48070a5d9357c
ryanbas21 <ryanbas21@gmail.com>
2016-10-27 21:01:05 -0700

    saving new changes, added controllers folder. added post route for messages

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
-var express = require('express');
+const express = require('express');
 
 // Create our app
-var app = express();
+const app = express();
 
 app.use(express.static('public'));
 
+
+app.post('/rooms/:roomid', )
+
+
+
+
+
+
+
+//listening on port 3000
 app.listen(3000, function () {
   console.log('Express server is up on port 3000');
 });

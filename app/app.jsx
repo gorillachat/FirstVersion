5fb8cb6bcc8d8055f13ac611484bac921cb260ed
Ahmad K <lance@codemosey.net>
2016-10-28 11:40:41 -0700

    adds lobby view, message GET and POST req functions.

--- app/app.jsx
+++ app/app.jsx
@@ -1,72 +1,135 @@
 import React, {Component} from 'react';
 const { render } = require('react-dom');
-
+const HOST = 'http://localhost:3000/';
 // TESTING RELATED --------------------- //
 const testMsgs = [
     {
-        "time": "2016-10-27T21:51:18-07:00",
-        "room": "1",
-        "creator": "ahmad",
-        "msgbody": "Sup Guise.",
+        "createdAt": "2016-10-27T21:51:18-07:00",
+        "roomId": "1",
+        "createdBy": "ahmad",
+        "msgBody": "Sup Guise.",
     },
     {
-        "time": "2016-10-27T20:51:18-07:00",
-        "room": "1",
-        "creator": "Stef",
-        "msgbody": "Hey.",
+        "createdAt": "2016-10-27T20:51:18-07:00",
+        "roomId": "1",
+        "createdBy": "Stef",
+        "msgBody": "Hey.",
     },
     {
-        "time": "2016-10-27T19:51:18-07:00",
-        "room": "1",
-        "creator": "Ryan",
-        "msgbody": "Whaddup.",
+        "createdAt": "2016-10-27T19:51:18-07:00",
+        "roomId": "1",
+        "createdBy": "Ryan",
+        "msgBody": "Whaddup.",
     },
     {
-        "time": "2016-10-27T18:51:18-07:00",
-        "room": "1",
-        "creator": "Mystery Man",
-        "msgbody": "???",
+        "createdAt": "2016-10-27T18:51:18-07:00",
+        "roomId": "1",
+        "createdBy": "Mystery Man",
+        "msgBody": "???",
     },
 ];
 
-
+const testRooms = [
+  {
+    "lat": "33.35",
+    "long": "-118.02",
+    "expires": "2016-10-27T18:51:18-07:00",
+    "name": 'CodeSmith Kitchen',
+    "creatorId": 'Ryan',
+    "_id": '5',
+  },
+  {
+    "lat": "33.35",
+    "long": "-118.02",
+    "expires": "2016-10-27T18:51:18-07:00",
+    "name": 'CodeSmith Kitchen',
+    "creatorId": 'Ryan',
+    "_id": '6',
+  }
+];
 class App extends Component {
     constructor() {
         super();
         this.state = {
             messages: testMsgs,
             view: 'room',
-            currentRoom: '',
+            currentRoomId: '9',
+            userId: '1',
+            roomList: testRooms,
         };
+    }
+    changeView(room) {
+      const newStateObj = {view: room};
+      this.setState(newStateObj);
+    }
+    addNewMessages(msgs) {
+      const newStateObj = { messages: this.state.messages.concat(msgs)};
+      this.setState(newStateObj);
+    }
+    joinRoom(roomId) {
+      const newStateObj = { view: 'room', currentRoomId: roomId };
+      this.setState(newStateObj);
     }
     render() {
         if (this.state.view === 'room') {
-          return <Room roomId={this.state.currentRoom} messages={this.state.messages} />
-        } else {
-          return <div>NO</div>
+          return <RoomView currentRoomId={this.state.currentRoomId} messages={this.state.messages} changeView={this.changeView.bind(this)} addNewMessages={this.addNewMessages.bind(this)}/>
+        } else if (this.state.view === 'lobby') {
+          return <Lobby roomList={this.state.roomList} joinRoom={this.joinRoom.bind(this)}/>
         }
     }
 }
-class Room extends Component {
+class RoomView extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return <Chatbox messages={this.props.messages} />
-  }
+    return (
+      <div id='chatroom-container'>
+        <h3>Room: {this.props.currentRoomId}</h3>
+        <button className='btn-back' onClick={() => this.props.changeView('lobby')}>Back to Lobby</button>
+        <Chatbox messages={this.props.messages} currentRoomId={this.props.currentRoomId} addNewMessages={this.props.addNewMessages}/>
+      </div>
+    )
+}
 }
 
 class Chatbox extends Component {
     constructor(props) {
       super(props);
     }
+    componentDidMount() {
+      const getReq = new XMLHttpRequest;
+      getReq.open("GET", HOST + '/rooms/' + this.props.currentRoomId);
+      getReq.addEventListener('load', () => {
+        console.log('Messages GOT', this.responseText);
+        // this.props.addNewMessages()
+      });
+      getReq.send();
+    }
+    postMsg() {
+      const msg = document.getElementById('newmsgbody').value;
+      const postReq = new XMLHttpRequest;
+      postReq.addEventListener('load', () => {
+        console.log('New Message Posted. ', this);
+        // this.props.addNewMessages()
+      });
+      postReq.setRequestHeader("Content-type", "application/json");
+      postReq.open("POST", HOST + '/rooms/' + this.props.currentRoomId);
+      postReq.send(JSON.stringify(msg));
+    }
     render() {
       const messagedivs = [];
       for (let i = 0; i < this.props.messages.length; i++) {
-        messagedivs.push(<Message data={this.props.messages[i]} />);
+        messagedivs.push(<Message key={`msg${i}`} data={this.props.messages[i]} />);
       }
       return  (
-        <div>{messagedivs}</div>
+        <div className="chatbox-container">
+          {messagedivs}
+          <form action={`/rooms/${this.props.currentRoomId}`}>
+            <input type='text' id='newmsgbody' name='msgbody'></input>
+            <button className='btn-postmsg' onClick={() => this.props.postMsg()}>Post</button>
+          </form>
+        </div>
       )
     }
 }
@@ -85,6 +148,41 @@ class Message extends Component {
   )
   }
 }
+
+class Lobby extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const roomDivs = [];
+    for (let i = 0; i < this.props.roomList.length; i++) {
+      roomDivs.push(<Room key={`room${i}`} data={this.props.roomList[i]} joinRoom={this.props.joinRoom} />);
+    }
+    return  (
+    <div className='lobby-container'>
+      <h2>Welcome to the Lobby</h2>
+      {roomDivs}
+    </div>
+  )
+  }
+}
+
+class Room extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    <div className='room-object'>
+      <span className='room-name'>{this.props.data.roomName}</span>
+      <span className='room-creator'>Creator: {this.props.data.creator}</span>
+      <span className='room-expires'>Expires: {this.props.data.expires}</span>
+      <button className='btn-joinroom' onClick={()=>this.props.joinRoom(this.props.data._id)}></button>
+    </div>
+  )
+  }
+}
+
 render(
   <App />,
   document.getElementById('app')

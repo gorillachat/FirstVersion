f6f222e9f7563e7a26cf01ad747b4df884a43fb6
srei <steferu@gmail.com>
2016-10-27 22:09:15 -0700

    Added sequelize to package.json. Established connection to port 5432, authenticated connection to database. Created main 3 models: ROOM, MSG, and USER.

---
+++ Schemas/Tables.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//require sequelize
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('postgres://localhost:5432/ChatRoomTables');
+
+const Room
+
+const Msg
+
+const User
+module.exports()

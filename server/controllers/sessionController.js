b60b88773f146ed2246d26ee41cc08b05f1a9845
ryanbas21 <ryanbas21@gmail.com>
2016-10-28 20:42:58 -0700

    we made cookies and sessions happen, woo!

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -2,7 +2,11 @@
 
 module.exports = {
   isLoggedIn : (req,res,next) => {
-    console.log(req);
-    // return (req.user[0].dataValues._id) ?  next() : res.redirect('/login');
+      console.log(req.cookies, 'this is the cookies on the session controller');
+     if (!req.cookies.user_id){
+       console.log('this should redirect!');
+       res.redirect('/login');
+       res.end();
+     } else next();
   }
 }
